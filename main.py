0c9f1270f69afd95b50f8eeea0fb164306dd9a2f
Mixaqi <mixaqimixaqi@mail.ru>
2022-10-24 21:00:46 +0300

    added numpy, alg for solution, test some methods from numpy and sympy

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import sympy
+from sympy import diff, symbols
+import numpy as np
+
+# Сгенерировать исходную систему, найти вектор-функцию F(X), Матрица Якоби W(x)
+
+amount_of_equations = int(input("Введите число уравнений системы: "))
+vector_function = np.arange(amount_of_equations).reshape(3, 4)
+print(vector_function)
